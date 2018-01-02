fe166e1d4d6b848c8b8515726b79bb07bbf6086f
Kang Cheng <me@chengkang.pw>
2018-01-02 14:59:48 +0000

    update

---
+++ template/src/ExamplePlugin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    \{{i18next.t('{{module_name}}.greeting')}}
+  </div>
+</template>
+<script>
+export default {
+  name: '{{name}}',
+  props: ['bus'],
+  computed: {
+    i18next () {
+      return this.bus.i18next
+    }
+  }
+}
+</script>
+
+              
---
+++ template/src/index.js
@@ -0,0 +1,23 @@
+import {{module_name}} from './ExamplePlugin'
+
+export default (options = {}) => {
+  return {
+    name: '{{module_name}}',
+    install: ({ registerComponent, i18n, renderAt }) => {
+      i18n('en', {
+        {{module_name}}: {
+          greeting: 'Hello, world!'
+        }
+      })
+      i18n('zh-CN', {
+        {{module_name}}: {
+          greeting: '你好，世界！'
+        }
+      })
+
+      registerComponent('{{name}}', {{module_name}})
+      renderAt('header.after', 'example-plugin')
+    },
+    options: options
+  }
+}
